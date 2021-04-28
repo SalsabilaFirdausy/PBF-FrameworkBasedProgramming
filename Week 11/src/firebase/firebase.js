8a42399849a3196841bced9a44bd8ac64f8337de
SalsabilaFirdausy <salsa.firdausy@gmail.com>
2021-04-28 11:14:30 +0700

    week 11

---
+++ Week 11/src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8AZzQpNw-Sn9qm8Wb5_HrIFZrJ5JH9iU",
+    authDomain: "pbf-week11.firebaseapp.com",
+    projectId: "pbf-week11",
+    storageBucket: "pbf-week11.appspot.com",
+    messagingSenderId: "37210534847",
+    appId: "1:37210534847:web:917e3d4e00e3903ee4e70a",
+    measurementId: "G-Y6LDK85CHM"
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
